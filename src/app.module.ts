29ac95563ff4b716b3099bcae3bee653117a0a81
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-09-08 11:21:44 -0300

    validacao das variaves de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { RestaurantModule } from './domain/restaurant/restaurant.module';
 import { ProductModule } from './domain/product/product.module';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation'
 
 
 @Module({
-  imports: [RestaurantModule, ProductModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate
+    }),
+    RestaurantModule,
+    ProductModule,
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
